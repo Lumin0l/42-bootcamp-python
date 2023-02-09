abbbdb9fa315595467998b0f1ccdf6d6195bf366
Imanol De La Iglesia Muriel <ide-la-i@c3r7s3.42urduliz.com>
2023-02-09 15:42:20 +0100

    ex04

--- ex04/operations.py
+++ ex04/operations.py
@@ -6,31 +6,40 @@
 #    By: ide-la-i <ide-la-i@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2023/02/07 15:37:38 by ide-la-i          #+#    #+#              #
-#    Updated: 2023/02/07 16:00:37 by ide-la-i         ###   ########.fr        #
+#    Updated: 2023/02/09 15:40:39 by ide-la-i         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 import sys
+import math
 
 def operations(num_1, num_2):
-	
+	print(f"Sum: {num_1+num_2}")
+	print(f"Difference: {num_1-num_2}")
+	print(f"Product: {num_1*num_2}")
+	if num_2 == 0:
+		print("Quotient: ERROR (division by zero)")
+		print("Remainder: ERROR ((modulo by zero)")
+	else:
+		print(f"Quotient: {num_1/num_2}")
+		print(f"Remainder: {num_1%num_2}")
 
 def main():
 	if len(sys.argv) < 3 or len(sys.argv) > 3:
-		print("You only need to type 2 numbers.")
+		print("You need to type exactly 2 numbers.")
 	else:
 		try:
 			num_1 = int(sys.argv[1])
 		except ValueError:
-			print("First number is not an integer")
+			print("Only integers")
 			return
 		try:
-			num_1 = int(sys.argv[2])
+			num_2 = int(sys.argv[2])
 		except ValueError:
-			print("Second number is not an integer")
+			print("Only integers")
 			return
-		operations(sys.argv[1], sys.argv[2])
+		operations(num_1, num_2)
 		
 
 if __name__ == '__main__':
-	sys.exit.(main())
+	sys.exit(main())
